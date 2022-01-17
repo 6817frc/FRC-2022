8690322e5e41656c689d53d0b42aeee358df20ce
KyleeCopeland <kyleecopeland@icloud.com>
2022-01-17 15:20:18 -0600

    Motor Controller
    Set up motor controller groups for left and right side and defined robotDrive

---
+++ base/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
+ * arcade steering.
+ */
+public class Robot extends TimedRobot {
+  private final PWMSparkMax leftFront = new PWMSparkMax(0);
+  private final PWMSparkMax leftBack = new PWMSparkMax (1);
+  private final PWMSparkMax rightFront = new PWMSparkMax(2);
+  private final PWMSparkMax rightBack = new PWMSparkMax (3);
+  private final MotorControllerGroup leftGroup = new MotorControllerGroup(leftFront, leftBack);
+  private final MotorControllerGroup rightGroup = new MotorControllerGroup(rightFront, rightBack);
+  private final DifferentialDrive robotDrive = new DifferentialDrive (leftGroup, rightGroup);
+  private final Joystick m_stick = new Joystick(0);
+
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    rightFront.setInverted(true);
+    leftBack.setInverted(true);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Drive with arcade drive.
+    // That means that the Y axis drives forward
+    // and backward, and the X turns left and right.
+    robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getX());
+  }
+}
