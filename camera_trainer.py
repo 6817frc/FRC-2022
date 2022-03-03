ceb0d56f0867c009b9223a2a91fb434174c945cb
KyleeCopeland <kyleecopeland@icloud.com>
2022-03-03 16:18:07 -0600

    added filters to image

--- camera_trainer.py
+++ camera_trainer.py
@@ -16,8 +16,10 @@ def mouseRGB(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN: #checks mouse left button down condition
         if not clickone:
             #collect first point
+            frame = cv2.boxFilter(frame, -1, (10,10))
             point[0] = [x,y]
             clickone = True
+            print(frame[x, y])
         else:
             #collect second point
             point[1] = [x,y]
@@ -26,18 +28,32 @@ def mouseRGB(event,x,y,flags,param):
             width = point[1][0] - point[0][0]
             height = point[1][1] - point[0][1]
 
-          
-            g = frame[y :y + height, x :x + width, 1:2]
+           
+            
         #colorsB = frame[y,x,0] #grabs coordinate of Blue
             #colorsG = frame[y,x,1] #grabs coordinate of Green
        # colorsR = frame[y,x,2] #grabs coordinate of Red
             #colors = frame[y,x] #grabs coordinate of mouse click   
         
            # g = frame[0:7, :, 1:2]
+            g = frame[y :y + height, x :x + width, 1:2]
             g_mean = np.mean(g)
             g_min = np.amin(g)
             g_max = np.amax(g)
 
+            b = frame[y :y + height, x :x + width, 0:1]
+            b_mean = np.mean(b)
+            b_min = np.amin(b)
+            b_max = np.amax(b)
+
+            r = frame[y :y + height, x :x + width, 0:-1]
+            r_mean = np.mean(r)
+            r_min = np.amin(r)
+            r_max = np.amax(r)
+
+           
+
+            cv2.rectangle(frame,(x, y),(x+width, y+height),(0, 0, 255), 2)
         #print("Red: ",colorsR) #telemetry/feedback for value gathered at mouse coordinate lines 17-21
            # print("Green: ",colorsG)
        # print("Blue: ",colorsB)
@@ -47,6 +63,13 @@ def mouseRGB(event,x,y,flags,param):
             print("Green Average Value: ", g_mean)
             print("Green Max Value: ", g_max)
 
+            print("Blue Min Value: ", b_min)
+            print("Blue Average Value: ", b_mean)
+            print("Blue Max Value: ", b_max)
+
+            print("Red Min Value: ", r_min)
+            print("Red Average Value: ", r_mean)
+            print("Red Max Value: ", r_max)
 
 cv2.namedWindow('mouseRGB')
 cv2.setMouseCallback('mouseRGB',mouseRGB)
