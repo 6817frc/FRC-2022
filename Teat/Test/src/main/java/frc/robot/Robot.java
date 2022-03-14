2e0aeec053d376aa40fabd3a316ef311729c2095
William-Clark0 <97919141+William-Clark0@users.noreply.github.com>
2022-03-14 17:15:21 -0500

    Updated it cause didn't update from GitHub app

--- Teat/Test/src/main/java/frc/robot/Robot.java
+++ Teat/Test/src/main/java/frc/robot/Robot.java
@@ -11,6 +11,9 @@ import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.XboxController;
+
+import edu.wpi.first.cameraserver.CameraServer;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
@@ -19,26 +22,46 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 public class Robot extends TimedRobot {
   float speedIncrement;
   float shotSpeed;
+  private final int MOTOR_OFF=0;
+  private final double TURNSPEED= 0.15;
   private final float SHOOTER_FINETUNE = (float) 0.01;
   private final float SHOOTER_COURSETUNE = (float) 0.05;
-  private final Spark leftFront = new Spark(1); //variable for front left motor
-  private final Spark leftBack = new Spark(2);
-  private final Spark rightFront = new Spark(3);
-  private final Spark rightBack = new Spark(4);
-  private final Spark Intake = new Spark(0);
+  private final Spark leftFront = new Spark(3); //variable for front left motor
+  private final Spark leftBack = new Spark(4);
+  private final Spark rightFront = new Spark(1);
+  private final Spark rightBack = new Spark(2);
+  private final Spark intake = new Spark(0);
   private final PWMSparkMax shooter = new PWMSparkMax(5);
   private final MotorControllerGroup leftGroup = new MotorControllerGroup(leftFront, leftBack);
   private final MotorControllerGroup rightGroup = new MotorControllerGroup(rightFront, rightBack);
   private final DifferentialDrive robotDrive = new DifferentialDrive (leftGroup, rightGroup);
   private final Joystick m_stick = new Joystick(0);
+  private final XboxController logiController = new XboxController(1); // 1 is the USB Port to be used as indicated on the Driver Station
 
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
+    rightBack.setInverted(true);
     rightFront.setInverted(true);
-    leftBack.setInverted(true);
+    CameraServer.startAutomaticCapture();
+    
+  }
+
+  @Override
+  public void autonomousInit () {
+  /*back up until xyvalue of camera reaches favored point that
+  we know works and then shot
+  later we could add picking up an additional ball before shooting 
+  */
+  //add start delay
+  //method one backup shoot high hoop    
+  // method two shoot low hoop then back up out of
+  // method three (start backwards)drive to ball pick it up turn around till we see the hoop then shoot
+  
+
+
   }
 
   @Override
@@ -47,19 +70,68 @@ public class Robot extends TimedRobot {
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
 	  
-    robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getX());
-    if (m_stick.getRawButtonPressed(3)) {
+    robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getZ());
+    if (m_stick.getRawButtonPressed(2)) {
     	speedIncrement= (speedIncrement==SHOOTER_COURSETUNE)?SHOOTER_FINETUNE:SHOOTER_COURSETUNE;
     }
-    if (m_stick.getRawButtonPressed(1)) {
+    if (m_stick.getRawButtonPressed(4)) {
     	shotSpeed += speedIncrement;
-    	 shooter.set(shotSpeed);
     }
-    if (m_stick.getRawButtonPressed(2)) {
+    if (m_stick.getRawButtonPressed(3)) {
     	shotSpeed -= speedIncrement;
-   	    shooter.set(shotSpeed);
     }
+      //shooter.set(m_stick.getRawButton(1)?shotSpeed:MOTOR_OFF);
+      //intake.set(m_stick.getRawButton(12)?shotSpeed:MOTOR_OFF);
+      shooter.set(logiController.getRawButton(6)?-.5:(logiController.getRawAxis(5)/4));
+      intake.set(logiController.getRawButton(5)?.5:-(logiController.getRawAxis(4)/4));
     SmartDashboard.putNumber("DB/Slider 0", shotSpeed);
   }
+  //function back up
+  public void backup(double speed,double duration) {
+    robotDrive.arcadeDrive(speed, 0);
+    //while duration(back up till camera can see the hoop)
+  }
+  //funtion go forward
+  public void forward(double speed,double duration) {
+    robotDrive.arcadeDrive(speed, 0);
+    //while duration
+  }
+  //function Shoot (high) w/camera
+  public void HighShotCam(int x,int y) {
+    int direction=(x>0)?1:-1;
+    // change x=0 to range
+    while (x!=0) {
+      robotDrive.arcadeDrive(0, TURNSPEED*direction);
+    }
+    double CamTo = ((y+100)/200);//change this code later for arc of ball and how that relates to 
+    shooter.set(CamTo);
+    intake.set(.5);
+  }  
+  //function Shoot (high) w/o camera
+  public void HighShot(int x,int y,double power) {
+    int direction=(x>0)?1:-1;
+    // change x=0 to range
+    while (x!=0) {
+      robotDrive.arcadeDrive(0, TURNSPEED*direction);
+    }
+    shooter.set(power);
+    intake.set(.5);
+  }  
+  //function shoot (low)
+  public void LowShot(int x,int y,double power) {
+    int direction=(x>0)?1:-1;
+    // change x=0 to range
+    while (x!=0) {
+      robotDrive.arcadeDrive(0, TURNSPEED*direction);
+    }
+    shooter.set(power);
+    intake.set(.5);
+  }  
+  //function pickup ball/intake
+  public void SuckySuckSuck(double speed,double duration) {
+    intake.set(speed);
+    //while duration
+  }
+
 
 }
